b6408e0fd2c000c590ef5bf89d227fbd063ad45f
Sajjadalgburi <dfgsajjad@gmail.com>
2024-01-23 10:07:26 -0700

    made the possible choice for the user

--- Main/index.js
+++ Main/index.js
@@ -1,10 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const generatedMarkdown = ({}) => {
-  return;
-};
-
 // Title
 // Description
 // Table of Contents
@@ -14,3 +10,50 @@ const generatedMarkdown = ({}) => {
 // test instructions
 // lastly questions => will have user github whcih will be a link
 // also email with "instructions on how to reach me with additional questions"
+
+const licenseOptions = [
+  "MIT License",
+  "GNU General Public License (GPL)",
+  "Apache License 2.0",
+  "BSD Licenses (2-Clause and 3-Clause)",
+  "Creative Commons Licenses",
+];
+
+inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "Enter Project Title",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Add Project Description",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "Add Installation Instructions",
+  },
+  {
+    type: "input",
+    name: "UsageInformation",
+    message: "Add Usage Information",
+  },
+  {
+    type: "input",
+    name: "Contribution",
+    message: "Add Contribution Guidelines",
+  },
+  {
+    type: "input",
+    name: "Test",
+    message: "Add Test Instructions",
+  },
+  {
+    type: "list",
+    name: "selectedLicense",
+    message: "Select A License",
+    choices: licenseOptions,
+  },
+]);
