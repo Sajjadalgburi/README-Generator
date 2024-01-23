1f265d06e632eadd6c5a7ab3af72dc251ff6ebcf
Sajjadalgburi <dfgsajjad@gmail.com>
2024-01-23 09:44:38 -0700

    psudo code...

--- Main/index.js
+++ Main/index.js
@@ -1,2 +1,16 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const generatedMarkdown = ({}) => {
+  return;
+};
+
+// Title
+// Description
+// Table of Contents
+// installation
+// usage information
+// conturbution information
+// test instructions
+// lastly questions => will have user github whcih will be a link
+// also email with "instructions on how to reach me with additional questions"
